26466431e250b549a83c6fcaabc31d248342a258
zurlomichele <zurlomichele1997@gmail.com>
2019-07-18 12:01:41 +0200

    Version 5.7.0 JavaDoc terminato.

--- src/main/java/com/example/progetto/controller/PaymentController.java
+++ src/main/java/com/example/progetto/controller/PaymentController.java
@@ -17,27 +17,42 @@ import org.springframework.web.server.ResponseStatusException;
 import java.util.Vector;
 
 
+/**
+ * This class implement the controller. This is the component
+ * invoked directly by the clients that deal with the main business logic and that
+ * can exist even without the presence of Model.
+ * <p>
+ * Following this, all the routes are structured following the GET or POST requests
+ * depending on the use cases.
+ * <p>
+ * Also note that each request (GET or POST that is) recalls the {@link PaymentService},
+ * within which the processing methods are defined acoording to the calls
+ * of the {@link PaymentController}.
+ */
 @RestController
 public class PaymentController {
     @Autowired
     private PaymentService paymentService;
 
+    //this route is used for show all data-set
     @GetMapping("/showdataset")
     public Vector<Payment> retrievePayments() {
         return paymentService.getPayments();
     }
 
+    //this route is used for show all metadata
     @GetMapping("/showmetadata")
     public Vector<Header> retrieveHeader() {
         return paymentService.getHeader();
     }
 
-
+    //this route is used to make statistics on data
     @GetMapping("/statistics/{fieldName}")
     public DataStatistics statistics(@PathVariable String fieldName) {
         return paymentService.statistics(fieldName, paymentService.getPayments());
     }
 
+    //this route is used to count all occurrences of certain elements
     @GetMapping(value = "/count/{fieldName}", produces = "application/json")
     public String count(@PathVariable String fieldName, @RequestParam(value = "value") String value) {
         Vector<Payment> payments = paymentService.getPayments();
@@ -46,62 +61,66 @@ public class PaymentController {
 
     }
 
+    //this route is used to filter the data
     @PostMapping(value = "/filter")
     public Vector<Payment> filter(@RequestBody String param) {
         JSONObject jsonObject = null;
-        boolean correctRequest = false;
+        boolean validRequest = false;
         try {
+            //represents the param from the json body in a structured json object
             jsonObject = (JSONObject) JSONValue.parseWithException(param);
         } catch (ParseException e) {
             System.out.println("Errore nel parsing JSON" + jsonObject);
         }
-        Vector<Payment> temp = paymentService.getPayments();
+        Vector<Payment> app = paymentService.getPayments();
         DataFiltering filterParam = new DataFiltering(null, null, null);
-        // single filter case
+        //if the filter requested in the json body does not include any logical operator
         filterParam.readFields(jsonObject);
         if (filterParam.getFieldName() != null && filterParam.getOperator() != null && filterParam.getValue() != null) {
-            correctRequest = true;
-            temp = paymentService.filter(temp, filterParam);
+            validRequest = true;
+            app = paymentService.filter(app, filterParam);
         }
 
-        // multiple filter cases: or - and
 
+        //if the filter requested in the json body does include OR, AND logical operator
         JSONArray jsonArray = (JSONArray) jsonObject.get("$or");
         if (jsonArray instanceof JSONArray) {
-            correctRequest = true;
-            // tempOr is used to store the partial filters
-            Vector<Payment> tempOr = new Vector<Payment>();
-            // the temp vector is emptied
-            temp = new Vector<Payment>();
+            validRequest = true;
+            // appOr is used to store the partial data filtered
+            Vector<Payment> appOr = new Vector<Payment>();
+            app = new Vector<Payment>();
 
             for (Object obj : jsonArray) {
                 filterParam.readFields(obj);
-                tempOr = paymentService.filter(paymentService.getPayments(), filterParam);
-                for (Payment item : tempOr) {
-                    if (!temp.contains(item))
-                        temp.add(item);
+                appOr = paymentService.filter(paymentService.getPayments(), filterParam);
+                for (Payment item : appOr) {
+                    if (!app.contains(item))
+                        app.add(item);
                 }
             }
         }
 
         jsonArray = (JSONArray) jsonObject.get("$and");
         if (jsonArray instanceof JSONArray) {
-            correctRequest = true;
+            validRequest = true;
             for (Object obj : jsonArray) {
                 filterParam.readFields(obj);
-                // iteration on .filter
-                temp = paymentService.filter(temp, filterParam);
+                // iteration on filter method, for each new cycle we use as vector to
+                //filter the output vector from the precedent cycle
+                app = paymentService.filter(app, filterParam);
             }
         }
 
-        if (!correctRequest) {
+        if (!validRequest) {
             System.out.println("Incorrect JSON body");
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Corpo del messaggio JSON errato");
         }
 
-        return temp;
+        return app;
     }
 
+    /*this used is used to get statistics on filtered data,
+    specifing filter paramenters in the JSON body*/
     @PostMapping(value = "/filter/statistics/{fieldName}")
     public DataStatistics filterStats(@RequestBody String param, @PathVariable String fieldName) {
         Vector<Payment> sample = new Vector<Payment>();
--- src/main/java/com/example/progetto/service/PaymentService.java
+++ src/main/java/com/example/progetto/service/PaymentService.java
@@ -13,6 +13,11 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.Math;
 
 
+/**
+ * This class implements various functions called by the controller, in fact the main
+ * features of our program are coded here, the Service of Spring boot contains the business
+ * logic of the application.
+ */
 @Service
 public class PaymentService {
     private static Vector<Payment> payments;
@@ -37,15 +42,27 @@ public class PaymentService {
         PaymentService.metadata = metadata;
     }
 
-    public DataStatistics statistics(String fieldName, Vector<Payment> sample) throws IllegalStateException
-    {
+    /**
+     * @param fieldName string representing a field, calculation are performed for every
+     *                  object on their specific attributes that match this string
+     * @param sample vector of a objects representing each row of the CSV
+     * @return object of type DataStatistics that encapsulates results of calculation performed
+     * @throws IllegalStateException message for the user that covers incorrect input
+     *                               specified for statistics on period
+     */
+    public DataStatistics statistics(String fieldName, Vector<Payment> sample) throws IllegalStateException {
         Method m = null;
         Vector<Integer> store = new Vector<Integer>();
+        /*these are two flags to check whether the field referrend by the user
+        on the URL is the programming period start o the programming period end*/
         boolean flagStart=false;
         boolean flagEnd=false;
         Vector<Object> results = new Vector<Object>();;
         int count =0;
         try {
+            /*methods to get PeriodStart and PeriodEnd field are encapsulated in the
+            method to get the field Period, so it is needed to change the name keeping
+            records whether if the users wanted to know the start or the end.*/
             switch (fieldName){
                 case "PeriodStart":
                     fieldName=fieldName.replace("PeriodStart","Period");
@@ -58,11 +75,19 @@ public class PaymentService {
             }
 
             for (Payment item : sample) {
+                /*using a reflection to get a method from the object class (representing
+                class Payment) named as the type of the field we need to get using the
+                String passed as parameter to the method.*/
                 m = item.getClass().getMethod("get" + fieldName);
                 if(fieldName.equals("Period")){
+                    /*invoke the getterMethod on the object item, which represent an element of the
+                    the vector passed as a parameter to the method.*/
                     Object paymentValue = m.invoke(item);
                     int Start= Integer.parseInt(getStartEnd(paymentValue)[0]);
                     int End = Integer.parseInt(getStartEnd(paymentValue)[1]);
+                    /*if the user requested statistics on the start of programming period
+                    we add the value extracted from the Vector passed as a parameter in
+                    another vector, same logic applied also for the end of programming period*/
                     if(flagStart)
                         store.add(Start);
                     else if(flagEnd)
@@ -70,20 +95,19 @@ public class PaymentService {
                     else{
                         throw new IllegalStateException("Devi specificare PeriodStart o PeriodEnd");
                     }
-                }
-                else{
+                } else{
                     Object paymentValue = m.invoke(item);
+                    /*we use the absolute value to correct some data in the csv that are wrong,
+                     such as negative payments that aren't possible as it doesn't make sense
+                    EU providing a negative Fund to region for its structural development.*/
                     int paymentValuefix = Math.abs((int)paymentValue);
                     store.add(paymentValuefix);
                 }
             }
-            count = store.size();
+            //we store the result of the math operation on object in a Vector of results
+            results = calculate(store);
 
-            results = calculate(count,store);
-
-        }
-
-        catch (IllegalAccessException e) {
+        } catch (IllegalAccessException e) {
             System.out.println("Il metodo " + m + " non ha accesso alla definizione del campo specifico");
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "non puoi accedere a questo metodo!:" + m);
         } catch (InvocationTargetException e) {
@@ -95,16 +119,29 @@ public class PaymentService {
             System.out.println("Il metodo " + fieldName + " non esiste");
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Il metodo" + fieldName + "non esiste");
         } catch (SecurityException e) {
-            System.out.println("Violazione di sicurezza");
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Violazione di sicurezza");
+            System.out.println("Violazione di sicurezza!");
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Violazione di sicurezza!");
         }
         return new DataStatistics((double)results.get(0), (int)results.get(1), (int)results.get(2), (double)results.get(3), (long)results.get(4));
     }
 
+    /**
+     *
+     *     This method implements the general filter logic used also to count the number of
+     *     occurrences.
+     *
+     * @param payments Vector of payments  representing the pool of object to filter
+     * @param param    object that structures  the specific filter parameters to use
+     * @return a vector of payments where each row respects filter parameters specified
+     * @throws IllegalStateException message for the user that covers incorrect input
+     *                               specified for statistics on period
+     */
     public Vector<Payment> filter(Vector<Payment> payments, DataFiltering param) throws IllegalStateException {
 
         Method m = null;
         Vector<Payment> out = new Vector<Payment>();
+        /*as before, also here we need to understand if the request of the user if
+        for the start or the end of the programming period.*/
         boolean flagStart=false,
                 flagEnd=false;
         switch(param.getFieldName()){
@@ -119,31 +156,33 @@ public class PaymentService {
         }
         try {
             for (Payment item : payments) {
+                /*using a reflection to get a method from the object class (representing
+                class Payment) named as the type of the field we need to get using the
+                String extracter from the getter of the object of type "DataFiltering"*/
                 m = item.getClass().getMethod("get" + param.getFieldName());
+                /*invoke the getterMethod on the object item, which represent an element of the
+                the vector passed as a parameter to the method.*/
                 Object paymentValue = m.invoke(item);
                 if(param.getFieldName().equals("Period")){
                     int Start= Integer.parseInt(getStartEnd(paymentValue)[0]);
                     int End = Integer.parseInt(getStartEnd(paymentValue)[1]);
                     if(flagStart){
                         paymentValue=Start;
+                        //this method implements the check of the conditional operators
                         if (PaymentService.check(paymentValue, param.getOperator(), param.getValue()))
                             out.add(item);
-                    }
-                    else if(flagEnd){
+                    } else if(flagEnd){
                         paymentValue=End;
                         if (PaymentService.check(paymentValue, param.getOperator(), param.getValue()))
                             out.add(item);
-                    }
-                    else{
+                    } else{
                         throw new IllegalStateException("Devi specificare PeriodStart o PeriodEnd");
                     }
-                }
-                else{
-                if (PaymentService.check(paymentValue, param.getOperator(), param.getValue()))
-                    out.add(item);}
+                } else{
+                    if (PaymentService.check(paymentValue, param.getOperator(), param.getValue()))
+                        out.add(item);}
             }
-        }
-         catch (IllegalAccessException e) {
+        } catch (IllegalAccessException e) {
             System.out.println("Il metodo " + m + " non ha accesso alla definizione del campo specifico");
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "non puoi accedere a questo metodo!:" + m);
         } catch (IllegalArgumentException e) {
@@ -166,14 +205,22 @@ public class PaymentService {
         return out;
     }
 
+    /**
+     * @param paymentValue Object containing a value extracted with the getter method from a row object.
+     *                     This is the first operand for the conditional comparision.
+     * @param operator      represent the operator to apply for the conditional comparison
+     * @param inputValue    represent the second operand for the conditional comparison specified
+     *                      by the user in the JSON body
+     * @return boolean stating if the condition is respected or not
+     */
     public static boolean check(Object paymentValue, String operator, Object inputValue) {
-
+        /*if the operand used to filter the dataset is a number, is possible
+        to access to a series of conditional operators*/
         if (paymentValue instanceof Number) {
             Integer InputValue = Integer.valueOf((String) inputValue);
             Integer PaymentValue =  (Integer)paymentValue;
 
-            switch (operator)
-            {
+            switch (operator) {
                 case "==":
                     return PaymentValue.equals(InputValue);
                 case ">":
@@ -188,6 +235,8 @@ public class PaymentService {
                         "operatore non conforme, gli operatori ammessi sono: ==, >, >=, <, <=");
             }
         }
+        /*if the operand used to filter the dataset is a String, is possible
+            to access only to the conditional operator of equivalence.*/
         else if (inputValue instanceof String && paymentValue instanceof String) {
             String inputString = (String) inputValue;
             String paymentString = (String) paymentValue;
@@ -201,43 +250,64 @@ public class PaymentService {
         return false;
     }
 
-        private String[] getStartEnd( Object paymentValue){
+    /**
+     *
+     * This function is used when we have to deal with the Programming Period.
+     *
+     * @param paymentValue object containing a value of Period extracted with the getter method
+     *                     from a row object.
+     *
+     * @return an array of string representing the Start and the End of the
+     *                     of the Period as separated values.
+     */
+    private String[] getStartEnd(Object paymentValue){
 
         String paymentValueFix=(String)paymentValue;
         String[] StartEnd = new String[2];
         StartEnd[0] = paymentValueFix.substring(0,4);
         StartEnd[1] = paymentValueFix.substring(5,9);
         return StartEnd;
+    }
+
+
+    /**
+     *
+     * This function implements the math operation needed to get the statistics requested
+     *
+     * @param store   vector representing the poll of values from which get the statistics
+     *
+     * @return vector of values of different types representing the results of the
+     *                operations performed
+     */
+    private Vector<Object> calculate(Vector<Integer> store) {
+        Integer count = store.size();
+        Integer min = store.get(0);
+        Integer max = store.get(0);
+        Double avg = (double) 0;
+        Long sum = (long) 0;
+        Double std = (double) 0;
+        Vector<Object> results = new Vector<Object>();
+        for (Integer item : store) {
+            avg += item;
+            if (item < min)
+                min = item;
+            if (item > max)
+                max = item;
+            sum += item;
         }
+        avg = avg / count;
 
-        private Vector<Object> calculate(Integer count, Vector<Integer> store){
-            Integer min = store.get(0);
-            Integer max = store.get(0);
-            Double avg = (double) 0;
-            Long sum = (long) 0;
-            Double std = (double) 0;
-            Vector<Object> results = new Vector<Object>();
-            for (Integer item : store) {
-                avg += item;
-                if (item < min)
-                    min = item;
-                if (item > max)
-                    max = item;
-                sum += item;
-            }
-            avg = avg / count;
+        for(Integer item:store){
+            std+=(item-avg)*(item-avg);
+        }
+        std = Math.sqrt(std/(count));
 
-            for(Integer item:store){
-                std+=(item-avg)*(item-avg);
-            }
-            std = Math.sqrt(std/(count));
-
-            results.add(avg);
-            results.add(min);
-            results.add(max);
-            results.add(std);
-            results.add(sum);
+        results.add(avg);
+        results.add(min);
+        results.add(max);
+        results.add(std);
+        results.add(sum);
         return results;
-        }
+    }
 
 }
